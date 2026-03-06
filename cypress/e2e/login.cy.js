183a058d8ff7ed03764fb5ad4ca8b31fe04c111f
Stephany <stephanycarolinem@gmail.com>
2026-03-06 17:13:53 -0300

    Implementa Page Object Model e adiciona teste de login inválido

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,31 +1,27 @@
-describe('Teste de Login', () => {
+import LoginPage from '../pages/loginPage'
+
+const loginPage = new LoginPage()
+
+describe('Login', () => {
 
   it('Login com sucesso', () => {
 
-    cy.visit('https://www.saucedemo.com')
-
-    cy.get('#user-name').type('standard_user')
-
-    cy.get('#password').type('secret_sauce')
-
-    cy.get('#login-button').click()
-
-    cy.url().should('include', 'inventory')
+    loginPage.visit()
+    loginPage.fillUsername('standard_user')
+    loginPage.fillPassword('secret_sauce')
+    loginPage.submit()
 
   })
 
-})
+  it('Login inválido', () => {
 
-it('Login inválido', () => {
+    loginPage.visit()
+    loginPage.fillUsername('usuario_errado')
+    loginPage.fillPassword('senha_errada')
+    loginPage.submit()
 
-  cy.visit('https://www.saucedemo.com')
+    cy.get('[data-test="error"]').should('be.visible')
 
-  cy.get('#user-name').type('usuario_errado')
-
-  cy.get('#password').type('senha_errada')
-
-  cy.get('#login-button').click()
-
-  cy.get('[data-test="error"]').should('be.visible')
+  })
 
 })
---
+++ cypress/pages/loginPage.js
@@ -0,0 +1,21 @@
+class LoginPage {
+
+  visit() {
+    cy.visit('https://www.saucedemo.com')
+  }
+
+  fillUsername(username) {
+    cy.get('#user-name').type(username)
+  }
+
+  fillPassword(password) {
+    cy.get('#password').type(password)
+  }
+
+  submit() {
+    cy.get('#login-button').click()
+  }
+
+}
+
+export default LoginPage
